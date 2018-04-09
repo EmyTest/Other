4970a7294f90b7cda4f8c1e4f3613fc416a3b643
Administrator <mg5851@126.com>
2018-04-09 14:49:23 +0800

    1

--- b02.py
+++ b02.py
@@ -1,7 +1,15 @@
 from __future__ import print_function
-for i in range(1,5):
-    for j in range(1,6):
-        if i==1 or i==4 or j==1 or j==5:
+# for i in range(1,5):
+#     for j in range(1,6):
+#         if i==1 or i==4 or j==1 or j==5:
+#             print('*',end =" ")
+#         else:
+#             print(' ',end=" ")
+#     print()
+
+for i in range(1,6):
+    for j in range(1,7-i):
+        if i==1 or i==5 or j==1 or j==7-i-1:
             print('*',end =" ")
         else:
             print(' ',end=" ")
