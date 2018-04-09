0c6151ec5985dac9d92989bb6ee53db04ace4043
Administrator <mg5851@126.com>
2018-04-09 15:09:56 +0800

    1

--- b02.py
+++ b02.py
@@ -6,11 +6,33 @@ from __future__ import print_function
 #         else:
 #             print(' ',end=" ")
 #     print()
+#
+# for i in range(1,6):
+#     for j in range(1,7-i):
+#         if i==1 or i==5 or j==1 or j==7-i-1:
+#             print('*',end =" ")
+#         else:
+#             print(' ',end=" ")
+#     print()
+for i in range(5):
+    for j in range(0,5-i):
+        print(end=" ")
+    for k in range(5-i,6):
+        print('*',end=" ")
+    print()
+print("-"*20)
 
-for i in range(1,6):
-    for j in range(1,7-i):
-        if i==1 or i==5 or j==1 or j==7-i-1:
+for i in range(5):
+    # for k in range():
+    for j in range(0,5-i):
+        print(end=" ")
+    for k in range(5-i,6):
+        if i==0 or k==5-i or i==4 or k==5:
             print('*',end =" ")
         else:
             print(' ',end=" ")
     print()
+
+print('^'*20)
+for i in range(5):
+    print(i)
