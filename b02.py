a27c0e96a56215d83d6ed25aef33d964ffd5d51b
Administrator <mg5851@126.com>
2018-04-11 11:19:51 +0800

    1

--- b02.py
+++ b02.py
@@ -138,19 +138,19 @@ from __future__ import print_function
 # for filename in os.listdir(path):
 #     #目录的路径和文件名拼接起来，得到了文件的绝路路径
 #     print(os.path.join(path,filename))
-import os
-def getdirpath(dirpath):
-    total = 0
-    allname = os.listdir(dirpath)
-    for name in allname:
-        fullpath = os.path.join(dirpath,name)
-        if os.path.isfile(fullpath):
-            total += os.path.getsize(fullpath)
-        elif os.path.isdir(fullpath):
-            total += getdirpath(fullpath)
-    return total
-size = getdirpath('F:\__MACOSX')
-print(size)
+# import os
+# def getdirpath(dirpath):
+#     total = 0
+#     allname = os.listdir(dirpath)
+#     for name in allname:
+#         fullpath = os.path.join(dirpath,name)
+#         if os.path.isfile(fullpath):
+#             total += os.path.getsize(fullpath)
+#         elif os.path.isdir(fullpath):
+#             total += getdirpath(fullpath)
+#     return total
+# size = getdirpath('F:\__MACOSX')
+# print(size)
 
 
 
@@ -158,27 +158,54 @@ print(size)
 ###数据类型的内置函数
 ####number 数值型 （int float  bool   complex     string 字符型       tuple 元组        list 列表         dictionary 字典   set集合
 
-#####字符串的简单操作
-str1='lo'
-str2='ve'
-print(str1+str2)
-print('lo'+'ve')
-
-print('#'*10)
-
-s='ilove python'
-print(s[-1])
+# #####字符串的简单操作
+# str1='lo'
+# str2='ve'
+# print(str1+str2)
+# print('lo'+'ve')
+#
+# print('#'*10)
+#
+# s='ilove python'
+# print(s[-1])
 
 #切片 包含开始不包含结尾
-print(s[1:5])
+# print(s[1:5])
 
 #help(str)
 #capitalize() 首字母大写 返回字符串
-s = 'i like python'
-print(s.capitalize())
+# s = 'i like python'
+# print(s.capitalize())
+
+#title()将每个单词首字母大写
+s = 'l love python'
+print(s.title())
+
+#upper()将所有字母变为大写
 
 
+#lower（）所有字母小写
+#swapcase（） 大小写互换
+s = 'l l LLL'
+print(s.swapcase())
+#len()计算字符串长度，不属于字符串的内建函数
+##find（） 查找指定字符串，找不到返回 -1,找到返回索引值
+# ##index（）查找指定字符串，找不到报错
+# s='asdfgkilkmb'
+# s1=s.find('a')
+# s2=s.index('0')
+# print(s1)
+# print(s2)
 
+##count 计算字符串出现的次数
+# s='sdffvbgmmm'
+# print(s.count('m'))
+
+#startswith() 检测是否有有制定的字母开头，返回布尔值
+#endswith（） 检测是否以制定的字母结束
+s='I an a baby '
+print(s.startswith('I'))
+print(s.endswith(' '))
 
 
 
