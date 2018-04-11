b97d069e1027045aab7d25d8ed2b14588ef30822
Administrator <mg5851@126.com>
2018-04-11 19:21:57 +0800

    1

---
+++ c01.py
@@ -0,0 +1,13 @@
+import  random
+import  math
+
+#输入函数
+num =  input("请输入一个三位数: ")
+# print(num)
+if num.isdigit() and 100<=int(num)<=999:
+    # 输入的函数是字符型， 不强制转换会报错
+
+        pass
+else:
+    print("请按规定输入")
+
